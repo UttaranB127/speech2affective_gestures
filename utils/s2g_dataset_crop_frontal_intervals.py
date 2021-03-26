51a629b71b2d7559ada3f9b00ddaa494abb9dae4
uttaranb127 <utta005@gmail.com>
2021-03-26 10:50:43 -0400

    added processing for s2g dataset

--- utils/s2g_dataset_crop_frontal_intervals.py
+++ utils/s2g_dataset_crop_frontal_intervals.py
@@ -7,9 +7,8 @@ import pandas as pd
 parser = argparse.ArgumentParser()
 parser.add_argument('-base_path', '--base_path', default='/media/uttaran/repo1/data/s2g',
                     help='base folder path of dataset')
-parser.add_argument('-output_path', '--output_path',
-                    default='output directory to save cropped intervals')
-parser.add_argument('-speaker', '--speaker', default='optionally, run only on specific speaker', required=False)
+parser.add_argument('-speaker', '--speaker', default=None,
+                    help='optionally, run only on specific speaker', required=False)
 
 args = parser.parse_args()
 
@@ -26,13 +25,14 @@ if __name__ == '__main__':
 
     for _, interval in tqdm(df_intervals.iterrows(), total=len(df_intervals)):
         try:
+            out_path = os.path.join(args.base_path, interval['speaker'], 'intervals')
+            os.makedirs(out_path, exist_ok=True)
             start_time = str(pd.to_datetime(interval['start_time']).time())
             end_time = str(pd.to_datetime(interval['end_time']).time())
             in_file = os.path.join(args.base_path, interval['speaker'], 'videos', interval['video_fn'])
-            out_file = os.path.join(args.output_path,
-                                    '{}_{}_{}-{}.mp4'.format(interval['speaker'], interval['video_fn'],
-                                                             str(start_time), str(end_time)))
-            print(in_file, out_file)
+            out_file = os.path.join(out_path, '{}_{}-{}.mp4'.format(interval['video_fn'],
+                                                                    str(start_time), str(end_time)))
+            print('Video: {}\nInterval: {}'.format(in_file, out_file))
             save_interval(in_file, str(start_time), str(end_time), out_file)
         except Exception as e:
             print(e)
